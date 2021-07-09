b0a16634497bce91ed5e80aebdb68613089c7446
Shaun <shaun@thenetninja.co.uk>
2021-07-09 12:47:39 +0100

    lesson-5

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('*.scss')
+  return src('shinobi/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['shinobi/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
